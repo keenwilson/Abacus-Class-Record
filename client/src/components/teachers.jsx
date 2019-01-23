658c14843f0d232f161783d4eb1bbdfa75405dea
Andrew Ulm <andrewulm.dev@gmail.com>
2019-01-22 20:26:26 -0600

    Teachers displaying to page from DB. Delete added, Edit almost finished.

--- client/src/components/teachers.jsx
+++ client/src/components/teachers.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Col, Row, Container } from "../components/common/Grid";
+import { List, ListItem } from "../components/common/List";
 import API from '../utils/API';
 
 class Teachers extends Component {
@@ -7,7 +8,8 @@ class Teachers extends Component {
         teachers: [],
         firstName: '',
         lastName: '',
-        email: ''
+        email: '',
+        editTeacher: false
     };
 
     componentDidMount() {
@@ -30,6 +32,10 @@ class Teachers extends Component {
         API.deleteTeacher(id)
             .then(res => this.loadTeachers())
             .catch(err => console.log(err));
+    };
+
+    editTeacher = id => {
+      // TODO: Add edit teacher function
     };
 
     handleInputChange = event => {
@@ -85,6 +91,69 @@ class Teachers extends Component {
                     </Col>
                     <Col size="md-6">
                         <h2 className="text-center">Show all teachers in database</h2>
+                        {/* Output All teachers Loop // Start */}
+                            {this.state.teachers.length ? (
+                                <List>
+                                    {this.state.teachers.map(teacher => (
+                                        <ListItem key={teacher._id}>
+                                            <strong>
+                                                {teacher.firstName},&nbsp;
+                                                {teacher.lastName},&nbsp;
+                                            </strong>
+                                            {teacher.email}
+                                            <button
+                                                className="btn-danger"
+                                                onClick={() => this.deleteTeacher(teacher._id)}>
+                                                X
+                                            </button>
+                                            <button
+                                                className="btn-success"
+                                                onClick={() => this.editTeacher(teacher._id)}>
+                                                X
+                                            </button>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            ) : (
+                                <strong>No Results to Display</strong>
+                            )}
+                        {/* Output All teachers Loop // End */}
+                    </Col>
+                </Row>
+                <Row>
+                    <Col size="md-6">
+                        Nothing
+                    </Col>
+                    <Col size="md-6">
+                        {/* TODO: Edit Teacher Form */}
+                        {/*
+                        {this.state.editTeacher ? (
+                            <form>
+                                <div className="formControl">
+                                    <label htmlFor="teacherFirstName">First Name:</label>
+                                    <input type="text" id="teacherFirstName" name="firstName" placeholder="First Name..."
+                                           value={this.state.firstName}
+                                           onChange={this.handleInputChange} />
+                                </div>
+                                <div className="formControl">
+                                    <label htmlFor="teacherLastName">Last Name:</label>
+                                    <input type="text" id="teacherLastName" name="lastName" placeholder="Last Name..."
+                                           value={this.state.lastName}
+                                           onChange={this.handleInputChange} />
+                                </div>
+                                <div className="formControl">
+                                    <label htmlFor="teacherEmail">E-Mail:</label>
+                                    <input type="text" id="teacherEmail" name="email" placeholder="E-Mail..."
+                                           value={this.state.email}
+                                           onChange={this.handleInputChange} />
+                                </div>
+                                <input type="submit" value="Add Teacher"
+                                       disabled={!(this.state.firstName && this.state.lastName && this.state.email)}
+                                       onClick={this.handleFormSubmit} />
+                            </form>
+                        ):(
+                            <p>Something Else</p>
+                        )}*/}
                     </Col>
                 </Row>
             </Container>
