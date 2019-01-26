f8fa71ca84e60a68b891dff0bc7633a5ba625d96
Keen Wilson <40321520+keenwilson@users.noreply.github.com>
2019-01-26 10:32:30 -0600

    create-react-buildpack

--- client/src/components/views/teacher/TeacherHome.jsx
+++ client/src/components/views/teacher/TeacherHome.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const TeacherHome = props => {
   const teacher = props.teacher;
@@ -27,6 +28,9 @@ const TeacherHome = props => {
           </li>
         ))}
       </ul>
+      <p>
+        <Link to="/teacher/attendance">Check Attendance</Link>
+      </p>
     </React.Fragment>
   );
 };
